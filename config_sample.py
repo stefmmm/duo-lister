c91429a6aa8e7a92e11436834c2347bde438b8ce
stefmmm <mail@stefmmm.xyz>
2020-04-12 21:33:09 +0200

    Added config sample

---
+++ config_sample.py
@@ -0,0 +1,11 @@
+# this is the config sample, rename this to config.py before use
+
+# this is where the webhook url stuff goes, keep it secret!
+webhook_id = 'YOUR WEBHOOK ID'
+webhook_secret = 'YOUR WEBHOOK SECRET'
+
+# This is the name of the webhook
+webhooker_name = 'Duo'
+
+# This is the avatar of the webhook
+webhooker_avatar_url = 'https://www.underconsideration.com/brandnew/archives/duolingo_2018_logo_social.png'
